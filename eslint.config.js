2a9d39c995e81d3a964b40e886bf62e386868ee0
Nath <36022252+BarbosaNath@users.noreply.github.com>
2025-04-25 11:21:29 -0300

    [JOB-24] chore: better configuration for stryker and vitest (#2)
    * chore: change vitest functions to be global
    
    * fix: change vitest configuration to not need browser

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,13 @@ import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
 
 export default defineConfig([
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}', '!./.stryker-tmp/*', '!./coverage/*'] },
+  {
+    files: [
+      '**/*.{js,mjs,cjs,ts,jsx,tsx}',
+      '!./.stryker-tmp/*',
+      '!./coverage/*',
+    ],
+  },
   {
     files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     languageOptions: { globals: globals.browser },
