81ec0e1d8aa5e8e23e19db968a712b470f941599
Matheus Eduardo <playerxmatt@gmail.com>
2025-05-06 18:57:11 -0300

    refactor: remove the need for id in the input component

--- src/components/input/types.ts
+++ src/components/input/types.ts
@@ -1,11 +1,13 @@
 import { ChangeEvent, HTMLInputTypeAttribute } from 'react';
 
-export interface InputProps {
-  label?: string;
-  id?: string;
+export interface InputBaseProps {
   name?: string;
   type?: HTMLInputTypeAttribute;
   value?: string;
   placeholder?: string;
   onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export interface InputProps extends InputBaseProps {
+  label?: string;
 }
