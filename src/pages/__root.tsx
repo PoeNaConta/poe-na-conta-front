33ffbb4e7ea3fbbf622e546eab68ea7e9dc521ce
Nath <playerxmatt@gmail.com>
2025-07-03 00:38:38 -0300

    feat: add reset.css

--- src/pages/__root.tsx
+++ src/pages/__root.tsx
@@ -1,6 +1,5 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
-import '../globals.css';
 
 export const Route = createRootRoute({
   component: () => (
